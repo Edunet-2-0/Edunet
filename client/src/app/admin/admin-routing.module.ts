9923480ad897509f265c528bf330c558f99883f9
Rayen RAFFA <raffa.rayen@gmail.com>
2020-05-18 17:23:26 +0100

    display admin dashboard with mock data

--- client/src/app/admin/admin-routing.module.ts
+++ client/src/app/admin/admin-routing.module.ts
@@ -1,9 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
+import { Map1Component } from './views/maps/map1/map1.component';
+import { ModalsComponent } from './views/modals/modals.component';
+import { BasicTableComponent } from './views/tables/basic-table/basic-table.component';
+import { Profile1Component } from './views/profile/profile1/profile1.component';
+import { ModuleWithProviders } from '@angular/core';
+import { NotFoundComponent } from './views/errors/not-found/not-found.component';
+import { Dashboard1Component } from './views/dashboards/dashboard1/dashboard1.component';
+
+
 import { AdminComponent } from './admin.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboards/v1' },
+  { path: 'dashboards', children:
+    [
+      { path: 'v1', component: Dashboard1Component },
+    ]
+  },
+  { path: 'profiles', children:
+    [
+      { path: 'profile1', component: Profile1Component },
+    ]
+  },
+  { path: 'tables', children:
+    [
+      { path: 'table1', component: BasicTableComponent },
+    ]
+  },
+  { path: 'maps', children:
+    [
+      { path: 'map1', component: Map1Component},
+    ]
+  },
+
+  { path: 'modals', component: ModalsComponent},
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  values: string[] = ['Tag 1', 'Tag 2', 'Tag 4'];
 
-  constructor() { }
+  specialPage: boolean;
+
+  private specialPages: any[] = [
+    '/pages/login',
+    '/pages/register',
+    '/pages/lock',
+    '/pages/pricing',
+    '/pages/single-post',
+    '/pages/post-listing'
+  ];
+
+  private currentUrl = '';
+
+  constructor(
+    private router: Router,
+    private location: Location
+  ) {
+
+    this.router.events.subscribe((route:any) => {
+      this.currentUrl = route.url;
+
+      this.specialPage = this.specialPages.indexOf(this.currentUrl) !== -1;
+    });
+
+  }
 
   ngOnInit(): void {
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
